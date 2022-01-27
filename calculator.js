364d9ef24d2a06fbe34687f31246baebe26af498
encompassingresidential <encompassing.residential@gmail.com>
2022-01-27 09:18:45 -0600

    CSS options to make the grid look like calculator.  Playing around.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,6 @@
 console.log("hello world ");
+
+let fieldEntryResult = document.getElementById("field-entry-result");
+
+// Web Dev Simplified stated that innerHTML is easily hacked, so use textContent
+fieldEntryResult.textContent = "Entry numbers and Values go here";
